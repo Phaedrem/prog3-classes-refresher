d7e8becee7ac3f9a12febdd2f2348025a1fd8add
Darren Bowers <darren.bowers@g.austincc.edu>
2022-02-01 16:09:25 -0600

    added calls to construct two circles, one at 0 and one at 3 as well as two function calls for displayCircle to test

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ Purpose: Classes and Objects refresher assignment
 #include "main.h"
 
 int main(int argc, char** argv){
+    Circle myCricle1;
+
+    Circle myCircle2(3);
+
+    displayCircle(&myCricle1);
+    displayCircle(&myCircle2);
     
     return 0;
 
